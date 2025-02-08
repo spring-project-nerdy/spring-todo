4a550d238645f93ff60962cb9626bf890509c7d4
jyung17 <iotaxixi@gmail.com>
2025-02-08 19:08:32 +0900

    로컬 스토리지를 이용한 액세스 토큰 저장

--- react-workspace/todo-react/src/service/ApiService.js
+++ react-workspace/todo-react/src/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
@@ -37,6 +45,8 @@ export function call(api, method, request) {
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO).then((response) => {
     if (response.token) {
+      // 로컬 스토리지에 토큰 저장
+      localStorage.setItem("ACCESS_TOKEN", response.token);
       // token이 존재하는 경우 Todo 화면으로 리디렉트
       window.location.href = "/";
     }
